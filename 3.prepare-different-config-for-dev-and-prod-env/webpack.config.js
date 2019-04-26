d1c54daa06bae12a09eec67e92d092bc6d3396ac
Jiadong Huang <rick.huang@kodak.com>
2019-04-26 16:32:44 +0800

    course 3 init

---
+++ 3.prepare-different-config-for-dev-and-prod-env/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path"); // Import node path module
+
+// html-webpack-plugin generates an HTML5 file that includes
+// all webpack bundles in the <bod> using <script> tag
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+// css-mini-extract-plugin extracts styles to an external stylesheet file,
+// and injects it into HTML file by creating a <link> tag in <head> element
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
+
+module.exports = {
+  mode: "development", // "production"
+  entry: "./src/index.js", // script entry point
+  output: {
+    filename: "bundle.js", // resulting script file name
+    path: path.join(__dirname, "dist") // output folder, should be an absolute path
+  },
+  devServer: {
+    contentBase: "./dist", // webpack-dev-server root
+    port: 3003 // port number
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: [{ loader: "html-loader" }]
+      },
+      {
+        test: [/\.(css)$|\.(scss)$/],
+        //use: [{ loader: "style-loader" }, { loader: "css-loader" }] // for inline style
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader"
+          //"sass-loader"
+        ] // for external style file
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[hash:8].[ext]" // Resulting image file
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/public/index.html", // source html file
+      filename: "./index.html" // resulting html file name
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.css" // resulting stylesheet file
+    })
+  ]
+};
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # web-dev-tutorial
 
-A tutorial for setting up a web application built top on node+express+webpack+babel+react
+This is a tutorial for setting up a web application built top on node+express+webpack+babel+react. It contains following contents:
+
+- Course 1 - [Set up a minimal web application hosted on express server](https://github.com/rick-hayek/web-dev-tutorial/blob/master/1.setup-a-minimal-node-based-web-app.md)
+- Course 2 - [Use webpack for bundling web assets](https://github.com/rick-hayek/web-dev-tutorial/blob/master/2.use-webpack-for-bundling-web-assets.md)
+- Course 3 - [Prepare different webpack configurations for development and production environments](https://github.com/rick-hayek/web-dev-tutorial/blob/master/3.prepare-different-config-for-dev-and-prod-env.md)
